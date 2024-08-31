09f13b088bd77403e07939beee2924721d695afb
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-08-31 16:36:47 +0300

    ımage entity class created

--- backend/src/main/java/com/TheMFG/backend/model/Image.java
+++ backend/src/main/java/com/TheMFG/backend/model/Image.java
@@ -1,4 +1,38 @@
 package com.TheMFG.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "images")
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long imageId;
+
+    @Column(unique = true)
+    private String imageName;
+
+    private String imageType;
+
+    @JsonIgnore
+    private String imagePath;
+
+    @Column(name = "image_url")
+    private String imageURL;
+
+    public Image(String imageName, String imageType, String imagePath, String imageURL){
+        super();
+        this.imageName = imageName;
+        this.imageType = imageType;
+        this.imagePath = imagePath;
+        this.imageURL = imageURL;
+    }
 }
