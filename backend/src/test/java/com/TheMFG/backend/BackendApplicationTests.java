6227d9af5199e8cadcdc558b5e9a379174485663
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-08-31 16:05:35 +0300

    project created

---
+++ backend/src/test/java/com/TheMFG/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheMFG.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
