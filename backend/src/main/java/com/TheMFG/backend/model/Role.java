0326721853e12e1c754a726fa42bf9b1fce3835c
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-09-02 13:01:44 +0300

    role entity class created

--- backend/src/main/java/com/TheMFG/backend/model/Role.java
+++ backend/src/main/java/com/TheMFG/backend/model/Role.java
@@ -1,4 +1,22 @@
 package com.TheMFG.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id",nullable = false)
+    private Integer roleId;
+
+    private String authority;
 }
