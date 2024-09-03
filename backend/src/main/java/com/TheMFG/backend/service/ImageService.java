c2fffd84f67bcf75320ae392575eb841b4e2480b
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-09-03 14:20:43 +0300

    ImageService created

---
+++ backend/src/main/java/com/TheMFG/backend/service/ImageService.java
@@ -0,0 +1,56 @@
+package com.TheMFG.backend.service;
+
+import com.TheMFG.backend.model.Image;
+import com.TheMFG.backend.repository.ImageRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Service
+@Transactional
+public class ImageService {
+    @Autowired
+    private ImageRepository imageRepository;
+
+    private static final File DIRECTORY = new File("C:\\Users\\Asus\\Desktop\\SpringBoot\\social-media-fullstack\\backend\\img");
+    private static final String URL = "http://localhost:8081/images";
+
+    public Image saveGifFromPost(Image image){
+        return imageRepository.save(image);
+    }
+
+    public Image uploadImage(MultipartFile file, String prefix) throws UnableToSavePhotoException{
+        try{
+            String extention = "." + file.getContentType().split("/")[1];
+            File img = File.createTempFile(prefix,extention,DIRECTORY);
+            file.transferTo(img);
+            String imageURL = URL + img.getName();
+            Image i = new Image(img.getName(), file.getContentType(),img.getPath(),imageURL);
+            Image saved = imageRepository.save(i);
+            return saved;
+        }catch (IOException e){
+            throw new UnableToSavePhotoException();
+        }
+    }
+
+    public byte[] downloadImage(String filename) throws UnableToResolvePhotoException{
+        try{
+            Image image = imageRepository.findByImageName(filename).get();
+            String filePath = image.getImagePath();
+            byte[] imageBytes = Files.readAllBytes(new File(filePath).toPath());
+            return imageBytes;
+        }catch (IOException e){
+            throw new UnableToResolvePhotoException();
+        }
+    }
+
+    public String getImageType(String filename){
+        Image image = imageRepository.findByImageName(filename).get();
+        return image.getImageType();
+    }
+}
