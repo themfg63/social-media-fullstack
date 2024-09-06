3c92013dbdf1a91cbd456d1962e3e776a44e44aa
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-09-06 16:14:14 +0300

    userservice class uptaded

--- backend/src/main/java/com/TheMFG/backend/service/UserService.java
+++ backend/src/main/java/com/TheMFG/backend/service/UserService.java
@@ -25,26 +25,25 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
+    private final RoleRepository  roleRepository;
     private final MailService mailService;
     private final PasswordEncoder passwordEncoder;
     private final ImageService imageService;
 
-    public User registerUser(RegistrationObject object){
+    public User registerUser(RegistrationObject dto){
         User user = new User();
-        user.setFirstName(object.getFirstName());
-        user.setLastName(object.getLastName());
-        user.setEmail(object.getEmail());
-        user.setDateOfBirth(object.getDt());
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setEmail(dto.getEmail());
+        user.setDateOfBirth(dto.getDt());
 
         String name = user.getFirstName() + user.getLastName();
-        boolean nameToken = true;
+        boolean nameTaken = true;
         String tempName = "";
-
-        while(nameToken){
+        while(nameTaken){
             tempName = generateUserName(name);
             if(userRepository.findByUsername(tempName).isEmpty()){
-                nameToken = false;
+                nameTaken = false;
             }
         }
 
@@ -68,6 +67,10 @@ public class UserService implements UserDetailsService {
         return name + generateNumber;
     }
 
+    public User getUserById(Integer userId){
+        return userRepository.findById(userId).orElseThrow(UserDoesNotExistException::new);
+    }
+
     public User getUserByUsername(String username){
         return userRepository.findByUsername(username).orElseThrow(UserDoesNotExistException::new);
     }
@@ -85,7 +88,7 @@ public class UserService implements UserDetailsService {
         user.setVerification(generateVerificationNumber());
 
         try{
-            mailService.sendMail(user.getEmail(), "Doğrulama Kodu ", "Doğrulama Kodunuz: " + user.getVerification());
+            mailService.sendMail(user.getEmail(),"Doğrulama Kodu","Doğrulama Kodunuz: " + user.getVerification());
             userRepository.save(user);
         }catch (Exception e){
             throw new EmailFaildToSendException();
@@ -93,9 +96,12 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
     }
 
-    public User verifyEmail(String username, Long code){
-        User user = userRepository.findByUsername(username).orElseThrow(UserDoesNotExistException::new);
+    private Long generateVerificationNumber(){
+        return (long) Math.floor(Math.random() * 100_000_000);
+    }
 
+    public User verifyEmail(String username,Long code){
+        User user = userRepository.findByUsername(username).orElseThrow(UserDoesNotExistException::new);
         if(code.equals(user.getVerification())){
             user.setEnabled(true);
             user.setVerification(null);
@@ -105,32 +111,28 @@ public class UserService implements UserDetailsService {
         }
     }
 
-    public User setPassword(String username,String password ){
+    public User setPassword(String username,String password){
         User user = userRepository.findByUsername(username).orElseThrow(UserDoesNotExistException::new);
         String encodedPassword = passwordEncoder.encode(password);
         user.setPassword(encodedPassword);
         return userRepository.save(user);
     }
 
-    private long generateVerificationNumber(){
-        return (long) Math.floor(Math.random() * 100_000_000);
-    }
-
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Kullanıcı Bulunamadı"));
-        Set<GrantedAuthority> authorities = user.getAuthorities().stream()
+    public UserDetails loadUserByUsername(String  username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Kullanıcı Bulunamadı"));
+        Set<GrantedAuthority> authorities = user.getAuthorities()
+                .stream()
                 .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
                 .collect(Collectors.toSet());
-        UserDetails userDetails = new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),authorities);
+        UserDetails userDetails = new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
         return userDetails;
     }
 
-    public User setProfileOrBannerPicture(String username, MultipartFile file,String prefix) throws UnableToSavePhotoException {
+    public User setProfileOrBannerPicture(String username, MultipartFile file, String prefix) throws UnableToSavePhotoException{
         User user = userRepository.findByUsername(username).orElseThrow(UserDoesNotExistException::new);
-
         Image image = imageService.uploadImage(file,prefix);
-
         if(prefix.equals("pfp")){
             user.setProfilePhoto(image);
         }else{
@@ -139,11 +141,13 @@ public class UserService implements UserDetailsService {
         return userRepository.save(user);
     }
 
-    public Set<User> followUser(String user, String follower){
-        User loggedInUser = userRepository.findByUsername(user).orElseThrow(UserDoesNotExistException::new  );
+    public Set<User> followUser(String  user, String follower){
+        User loggedInUser = userRepository.findByUsername(user).orElseThrow(UserDoesNotExistException::new);
+
         Set<User> followingList = loggedInUser.getFollowing();
         User followedUser = userRepository.findByUsername(follower).orElseThrow(UserDoesNotExistException::new);
-        Set<User> followersList = followedUser.getFollowers()   ;
+
+        Set<User> followersList = followedUser.getFollowers();
 
         followingList.add(followedUser);
         loggedInUser.setFollowing(followingList);
@@ -159,22 +163,24 @@ public class UserService implements UserDetailsService {
 
     public Set<User> retrieveFollowingList(String username){
         User user = userRepository.findByUsername(username).orElseThrow(UserDoesNotExistException::new);
-        return  user.getFollowing();
+        return user.getFollowing();
     }
 
     public Set<User> retrieveFollowersList(String username){
         User user = userRepository.findByUsername(username).orElseThrow(UserDoesNotExistException::new);
-        return user.getFollowers()  ;
+        return user.getFollowers();
     }
 
     public String verifyUsername(FindUsernameDTO dto){
-        User user = userRepository.findByEmailOrPhoneOrUsername(dto.getEmail(), dto.getPhone(), dto.getUsername())
-                .orElseThrow(UserDoesNotExistException::new);
+        User user = userRepository.findByEmailOrPhoneOrUsername(
+                dto.getEmail(), dto.getPhone(), dto.getUsername()
+        ).orElseThrow(UserDoesNotExistException::new);
         return user.getUsername();
     }
 
     public User getUsersEmailAndPhone(FindUsernameDTO dto){
-        return userRepository.findByEmailOrPhoneOrUsername(dto.getEmail(),dto.getPhone(),dto.getUsername())
-                .orElseThrow(UserDoesNotExistException::new);
+        return userRepository.findByEmailOrPhoneOrUsername(
+                dto.getEmail(),dto.getPhone(),dto.getUsername()
+        ).orElseThrow(UserDoesNotExistException::new);
     }
 }
